00d2af958da7c9e05e6301ed58114266f18a3c8e
Devarshi Trivedi <devarshi.trivedi@walter.pride.improwised.dev>
2023-02-13 14:16:44 +0530

    new concurransy started

--- Devarshi_Trivedi/Mini_Projects/GO/Concurrancy/book.go
+++ Devarshi_Trivedi/Mini_Projects/GO/Concurrancy/book.go
@@ -2,18 +2,16 @@ package main
 
 import "fmt"
 
-
 type Book struct {
-	ID int
-	Title string
-	Author string
+	ID            int
+	Title         string
+	Author        string
 	YearPublished int
 }
 
-func(b Book) String() string {
+func (b Book) String() string {
 	return fmt.Sprintf(
-			"Title:\t\t%q\n"+
-				"Author:\t\t%q\n"+
-					"Published:\t%v\n", b.Title, b.Author, b.YearPublished)
-	)
+		"Title:\t\t%q\n"+
+			"Author:\t\t%q\n"+
+			"Published:\t%v\n", b.Title, b.Author, b.YearPublished)
 }
