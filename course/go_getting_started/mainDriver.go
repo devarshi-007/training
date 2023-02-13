0427a87156e815f2082b731991d63817c6b8b4cb
Jay Ponda <pondajay637@gmail.com>
2023-02-13 12:24:34 +0530

    add practice of go:getting started course

---
+++ course/go_getting_started/mainDriver.go
@@ -0,0 +1,18 @@
+package main
+
+import "main/webService"
+
+func main() {
+	// module 1
+	// module1.Main()
+
+	// module 2
+	// module2.Main()
+
+	// module 3
+	// module3.Main()
+
+	// web project
+	webService.Main()
+
+}
