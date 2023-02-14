7762ebe5cef62ef0e544e0d434a687b2db6aa9a4
jinal.gajjar <jinal.gajjar@improwised.com>
2023-02-14 11:46:16 +0530

    Go Practice Added

---
+++ golang_practices/helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// import "fmt"
+
+func helloworld() {
+
+	var a int
+	a = 10
+	fmt.Println("Hello world", a)
+
+}
---
+++ golang_practices/variable1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func variable() {
+	var i int
+	i = 20
+	fmt.Println(i)
+}
