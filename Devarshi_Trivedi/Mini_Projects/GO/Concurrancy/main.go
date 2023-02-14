eb7e261c36a6a1b23db6fdbd5af992afc3f537a1
Devarshi Trivedi <devarshi.trivedi@walter.pride.improwised.dev>
2023-02-14 13:25:23 +0530

    o

--- Devarshi_Trivedi/Mini_Projects/GO/Concurrancy/main.go
+++ Devarshi_Trivedi/Mini_Projects/GO/Concurrancy/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -11,33 +12,39 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	//wg := sync.WaitGroup{}
-	//wg := &sync.WaitGroup{} //we gonna passing this around in app , so we can pass pointer
-	count := 1
+	wg := &sync.WaitGroup{} //we gonna passing this around in app , so we can pass pointer
+	// count := 1
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		go func(id int) {
+		//wg.Add(1) //method-1
+		wg.Add(2) //method-2
+		go func(id int, wg *sync.WaitGroup) {
 			if b, ok := queryCache(id); ok {
-				fmt.Println("Count: ", count)
-				count++
+				// fmt.Println("Count: ", count)
+				// count++
 				fmt.Println("from cache")
 				fmt.Println(b)
 			}
-		}(id)
-		go func(id int) {
+			wg.Done()
+		}(id, wg)
+		//wg.Add(1) //method-1
+		go func(id int, wg *sync.WaitGroup) {
 			if b, ok := queryDatabase(id); ok {
-				fmt.Println("Count: ", count)
-				count++
+				// fmt.Println("Count: ", count)
+				// count++
 				fmt.Println("from database")
 				fmt.Println(b)
 			}
-		}(id)
+			wg.Done()
+		}(id, wg)
 		//fmt.Println("Book not found of id", id)
 
 		//Right way of doing this is waitgroups
-		time.Sleep(150 * time.Millisecond)
+		//time.Sleep(150 * time.Millisecond)
 		//DEMO WORKING ONLY BECAUSE OF THIS, REMOVE THIS AND IT DOSEN'T WORK
 	}
 	//time.Sleep(2 * time.Second)
+	wg.Wait() //wait till no activities
 }
 
 func queryCache(id int) (Book, bool) {
@@ -49,7 +56,7 @@ func queryDatabase(id int) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
-			cache[id] = b
+			//cache[id] = b
 			return b, true
 		}
 	}
