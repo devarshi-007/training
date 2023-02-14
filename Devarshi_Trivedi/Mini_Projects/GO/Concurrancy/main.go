e3457405d7d3055d3636c1155c43e45d5603f517
Devarshi Trivedi <devarshi.trivedi@walter.pride.improwised.dev>
2023-02-14 16:09:28 +0530

    concurancy added

--- Devarshi_Trivedi/Mini_Projects/GO/Concurrancy/main.go
+++ Devarshi_Trivedi/Mini_Projects/GO/Concurrancy/main.go
@@ -14,31 +14,48 @@ func main() {
 	//wg := sync.WaitGroup{}
 	wg := &sync.WaitGroup{} //we gonna passing this around in app , so we can pass pointer
 	m := &sync.RWMutex{}
+	cacheCh := make(chan Book)
+	dbCh := make(chan Book)
 	// count := 1
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 		//wg.Add(1) //method-1
 		wg.Add(2) //method-2
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryCache(id, m); ok {
 				// fmt.Println("Count: ", count)
 				// count++
-				fmt.Println("from cache")
-				fmt.Println(b)
+				// fmt.Println("from cache")
+				// fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, cacheCh)
 		//wg.Add(1) //method-1
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryDatabase(id, m); ok {
 				// fmt.Println("Count: ", count)
 				// count++
-				fmt.Println("from database")
-				fmt.Println(b)
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, dbCh)
 		//fmt.Println("Book not found of id", id)
+
+		go func(cacheCh, dbCh <-chan Book) {
+			select {
+			case b := <-cacheCh:
+				fmt.Println("from cache")
+				fmt.Println(b)
+				<-dbCh
+			case b := <-dbCh:
+				fmt.Println("from Database")
+				fmt.Println(b)
+			}
+		}(cacheCh, dbCh)
 
 		//Right way of doing this is waitgroups
 		time.Sleep(150 * time.Millisecond)
@@ -55,13 +72,10 @@ func queryCache(id int, m *sync.RWMutex) (Book, bool) {
 	return b, ok
 }
 
-func queryDatabase(id int, m *sync.RWMutex) (Book, bool) {
+func queryDatabase(id int) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
-			m.Lock()
-			cache[id] = b
-			m.Unlock()
 			return b, true
 		}
 	}
