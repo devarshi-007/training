3be666520c7f2dde44a3a39ce6bf9502fdecc662
Jay Ponda <pondajay637@gmail.com>
2023-02-17 14:01:02 +0530

    add: testing_go_applications

---
+++ golang/course/testing_go_applications/messsages/messages.go
@@ -0,0 +1,11 @@
+package messsages
+
+import "fmt"
+
+func Greet(name string) string {
+	return fmt.Sprintf("hello, %v\n", name)
+}
+
+func depart(name string) string {
+	return fmt.Sprintf("Goodbye, %v\n", name)
+}
