b33577757968ad951ede55d33fbc884d1eb7907b
Jay Ponda <jay.ponda@improwised.com>
2023-04-04 11:39:56 +0530

    complete javascript fundamentals

--- javascript/course/JavaScript_fundamentals/web/index.js
+++ javascript/course/JavaScript_fundamentals/web/index.js
@@ -5,6 +5,44 @@ let timeoutId = setTimeout(function () {
 
 console.log("hii");
 
-console.log(window.location, window.location.href)
+console.log(window.location, window.location.href);
 
-console.log(document.getElementsByClassName('row')) 
+console.log(document.getElementsByClassName("row"));
+
+let xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(JSON.parse(this.responseText));
+  }
+};
+
+xhttp.open("GET", "https://list.ly/api/v4/users/1", true);
+xhttp.send();
+
+let promiss = $.get("https://list.ly/api/v4/users/1");
+
+promiss.then(
+  (data) =>
+    (document.getElementById("here").textContent =
+      ("success: ", JSON.stringify(data))),
+  (err) =>
+    (document.getElementById("here").textContent =
+      ("error: ", JSON.stringify(err)))
+);
+
+let form = document.getElementsByTagName("form")[0];
+document.addEventListener("submit", (e) => {
+  let field = form.elements["just"];
+  console.log(
+    "submitted",
+    field.value,
+    field.innerText,
+    document.getElementById("here").innerText
+  );
+  if (field.value.trim() == "") {
+    field.style.borderColor = "red";
+    field.focus();
+    e.preventDefault();
+  }
+});
--- javascript/course/Javascipt_getting_started/index.js
+++ javascript/course/Javascipt_getting_started/index.js
@@ -1,4 +1,4 @@
-const print = console.log.bind(this)
+const println = console.log.bind(this)
 
 // strings
 
@@ -11,19 +11,81 @@ let str1 = "this is\
 \n new`\
 \n"
 
-print(str, typeof str, str == str1, str === str1)
+println(str, typeof str, str == str1, str === str1)
 
 // numeric
 
 let p = 10
 let k
-print(p++, p)
-print(++p, p)
+println(p++, p)
+println(++p, p)
 
-print(p--, p)
-print(--p, p)
+println(p--, p)
+println(--p, p)
 
-print(3 + 3 * 3)
+println(3 + 3 * 3)
 
 k = 10.3
-print(k+p)
+println(k+p)
+
+println(5.0 == 5, "5.0" === 5)
+println(5.0 == 5, "5.0" !== 5)
+
+if (0 || undefined || null || NaN || false) {
+ println(0)
+}
+println(0 ,"as false")
+
+if (+(1.1 + 10.333).toFixed(2) === 11.43){
+    println("ok")
+}
+
+if(undefined || "false"){
+    println("true")
+}
+
+function name5(newname = null){
+    
+    return String(newname).toUpperCase()
+}
+let myFun = name5
+
+println(myFun(1), name5(10))
+
+let symbol = Symbol()
+
+let obj = {
+    name: "k",
+    "full name": "j k",
+    [symbol]: "hii"
+}
+
+println(obj, obj["full name"], obj.name)
+println(obj, obj["full name"], obj.symbol)
+
+obj["full name"]
+
+// document.getElementById("main").setAttribute("data-new", "new-data")
+
+
+function  makeObj(name = "", surname = "") {
+    if ((name && name.trim() != "")  && (surname && surname.trim() != ""))
+    return {
+        name: name,
+        surname: surname,
+        getFullName: function(){
+            return `${this.name} ${this.surname}`
+        }
+    };
+
+    throw Error("name and surname should be string")
+}
+
+obj = makeObj("jay", "ponda")
+println(obj.getFullName(), obj.name , !obj, !!obj)
+
+println(eval("1 + 2 * 3 ** 2"))
+
+const arr = [1, 2]
+
+println(typeof arr, Array.isArray(arr), arr.length, arr.values().next(), arr.values(), arr.values().next())
