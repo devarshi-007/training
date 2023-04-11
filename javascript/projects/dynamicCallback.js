9edc0ebfc95c5f625665a266a8f2358f88716271
Jay Ponda <jay.ponda@improwised.com>
2023-04-11 19:58:53 +0530

    add dynamicCallback.js

--- javascript/projects/dynamicCallback.js
+++ javascript/projects/dynamicCallback.js
@@ -1,11 +1,26 @@
-function add(a, b, callback) {
-  callback(a + b);
-}
-
 function mul(a, b, callback) {
-  callback(a * b);
+  setTimeout(() => {
+    callback(a * b);
+  }, 500);
 }
 
-function opr(sum = []) {}
+function makeCal(...args) {
+  let b = [];
+  args.forEach((elm, index) => {
+    if (index % 2 == 0) {
+      if (index + 1 < args.length) b.push(elm * args[index + 1]);
+      else b.push(elm);
+    }
+  });
+  console.log(b);
 
-opr(2, 1);
+  let sum = 0;
+
+  b.forEach((elm) => {
+    sum += elm;
+  });
+
+  console.log(sum);
+}
+
+makeCal(1, 2, 3, 4, 5);
